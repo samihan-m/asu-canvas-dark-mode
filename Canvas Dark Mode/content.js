02b3970969bf050754e3e6f496075541f46d6f9a
PikaGoku <23440673+PikaGoku@users.noreply.github.com>
2020-10-29 11:25:54 -0700

    Version 1.0.0
    Everything looks pretty good. There's a little bit of jank here and there but I'll work on it.

---
+++ Canvas Dark Mode/content.js
@@ -0,0 +1,44 @@
+//This targets class display boxes with the background images in them and removes them.
+function removeClassBoxBackgroundImages() {
+	var classDisplayBoxes = document.getElementsByClassName("Grouping-styles__hero Grouping-styles__heroHover");
+	for (var i = 0; i < classDisplayBoxes.length; i++) {
+		classDisplayBoxes[i].style = "";
+	}
+}
+
+//This targets class display box titles and reduces them to more cleaner titles. Cuts the title after the first colon.
+function cleanDisplayBoxes() {
+	var classDisplayTitles = document.getElementsByClassName("Grouping-styles__title");
+	for (var i = 0; i < classDisplayTitles.length; i++) {
+	var oldText = classDisplayTitles[i].textContent;
+		var breakPoint = oldText.indexOf(":");
+		if(breakPoint > 0) {	
+			//error checking - dont want the substring to be 0,0 or 0,-1
+			classDisplayTitles[i].textContent = oldText.substring(0,breakPoint);
+			classDisplayTitles[i].style.fontSize = "200%";
+		}
+	
+	}
+}
+
+//This targets the announcement type details at the top of each announcement and keeps everything after the ')'
+function cleanAnnouncementType() {
+	var classAnnouncementMessages = document.getElementsByClassName("PlannerItem-styles__type");
+	for (var i = 0; i < classAnnouncementMessages.length; i++) {
+	var oldText = classAnnouncementMessages[i].textContent;
+		var breakPoint = oldText.indexOf(")");
+		if(breakPoint > -1 && breakPoint < oldText.length -1) {	
+			//error checking - dont want the substring to be -1,0 or 1,1
+			classAnnouncementMessages[i].textContent = oldText.substring(breakPoint + 1,oldText.length);
+		}
+		classAnnouncementMessages[i].style.color = "#bdbdbd";
+	}
+}
+
+var updateTime = 300;
+
+setInterval(cleanDisplayBoxes, updateTime);
+setInterval(removeClassBoxBackgroundImages, updateTime);
+setInterval(cleanAnnouncementType, updateTime);
+//If i can find a better way to make these functions happen (like in the css stylesheet or something) then do that.
+//I don't like the idea of functions running constantly.
---
+++ README.md
@@ -0,0 +1,2 @@
+# asu-canvas-dark-mode
+ A Chrome extension. Enables a dark theme for ASU's Canvas pages. Also slightly redesigns the page for a sleeker look. Or something like that.
